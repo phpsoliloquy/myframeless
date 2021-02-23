dc0113e4dedb79443e3a5ee12a77ec38f4ca8272
mwgn <phpsoliloquy@132134.neomailbox.net>
2021-02-23 20:43:28 +0700

    First Commit

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php declare(strict_types = 1);
+
+/*
+Use the Magic Constant to return the current working directory then concatenate the string to move a directory up then into the src directory for bootstrapping.
+*/
+require __DIR__ . '/../src/Bootstrap.php';
