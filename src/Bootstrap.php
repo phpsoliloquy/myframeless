f069a0c7f55156a6691eed29aef5c907e91ead95
mwgn <phpsoliloquy@132134.neomailbox.net>
2021-03-01 23:46:32 +0700

    Whoops error package added to Composer.

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,2 +1,26 @@
 <?php declare(strict_types = 1);
-echo "Permissions again.";
+echo "Text to test screen output.";
+
+namespace myframeworkless;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+// When pushed to the production server chance the line below to "production".
+$environment = 'development';
+
+/**
+* Register the error handler
+*/
+$whoops = new \Whoops\Run;
+if ($environment !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e){
+        echo 'Todo: Friendly error page and send an email to the developer';
+    });
+}
+$whoops->register();
+
+throw new \Exception;
